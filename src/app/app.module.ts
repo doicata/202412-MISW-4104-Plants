122efd9266cc6a786be1a15dc997a50b96f648ec
DANIEL OICATA HERNANDEZ <d.oicata@uniandes.edu.co>
2024-11-03 18:03:45 -0500

    Agregar header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlantsModule } from './plants/plants.module';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      FooterComponent
+      FooterComponent,
+      HeaderComponent
    ],
   imports: [
     BrowserModule,
